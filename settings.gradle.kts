306c66150bb3cad98c668a2f58bc9a7c015fe494
metacomputingchoi <choi@metacomputing.co.kr>
2025-07-08 23:57:42 +0900

    Build Success using naming engine

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,4 +27,8 @@ dependencyResolutionManagement {
 
 rootProject.name = "NameSpring"
 include(":app")
+
+// Get the Naming Engine from https://github.com/metacomputing-dev/NamingEngine
+// TODO add it as a submodule
 include(":namingengine")
+project(":namingengine").projectDir = File("../../Private.NamingEngine/NamingEngine/namingengine")
